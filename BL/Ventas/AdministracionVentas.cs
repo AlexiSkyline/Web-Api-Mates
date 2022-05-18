5dbf6c7297dca2313c77a97736410c8ab04e8cb9
AlexiSkyline <ilegal_sprite@outlook.com>
2022-05-18 00:28:50 -0500

    Agregamos la ruta para eliminar Ventas de la BD

--- BL/Ventas/AdministracionVentas.cs
+++ BL/Ventas/AdministracionVentas.cs
@@ -76,6 +76,64 @@ public class AdministracionVentas {
         return resultado;
     }
 
+    public async Task<VentasResponse> EliminarVentas( Guid? id ) {
+        VentasResponse resultado = new VentasResponse();
+
+        if( id != null ) {
+            using( var conexion = new SqlConnection( ContextDB.CadenaConexion ) ) {
+                conexion.Open();
+
+                var comando = new SqlCommand {
+                    Connection = conexion,
+                    CommandText = "[dbo].[AdminVentas]",
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                comando.Parameters.AddWithValue( "@Id", id );
+                comando.Parameters.AddWithValue( "@Opcion", "Eliminar" );
+
+                SqlParameter exito = new SqlParameter();
+                exito.ParameterName = "@Exito";
+                exito.SqlDbType = System.Data.SqlDbType.Bit;
+                exito.Direction = System.Data.ParameterDirection.Output;
+
+                comando.Parameters.Add( exito );
+
+                SqlParameter mensaje = new SqlParameter();
+                mensaje.ParameterName = "@Mensaje";
+                mensaje.SqlDbType = System.Data.SqlDbType.VarChar;
+                mensaje.Direction = System.Data.ParameterDirection.Output;
+                mensaje.Size = 4000;
+
+                comando.Parameters.Add( mensaje );
+
+                var lectura = await comando.ExecuteReaderAsync();
+
+                while( lectura.Read() ) {
+                    resultado.Id = lectura.GetGuid( "Id" );  
+                    resultado.Fecha = lectura.GetDateTime( "Fecha" );   
+                    resultado.IdVendedor = lectura.GetGuid( "IdVendedor" );  
+                    resultado.IdCliente = lectura.GetGuid( "IdCliente" );
+                    resultado.Folio = lectura.GetInt32( "Folio" );
+                    resultado.IdEmpresa = lectura.GetGuid( "IdEmpresa" );
+                    resultado.Total = lectura.GetDecimal( "Total" );
+                    resultado.Iva = lectura.GetDecimal( "Iva" );
+                    resultado.SubTotal = lectura.GetDecimal( "SubTotal" );
+                    resultado.PagoCon = lectura.GetString( "PagoCon" );             
+                }
+
+                conexion.Close();
+                resultado.Exito = (bool) exito.Value; 
+                resultado.Mensaje = (string) mensaje.Value; 
+            }
+        } else {
+            resultado.Exito = false;
+            resultado.Mensaje = "El ID es requerido";
+        }
+
+        return resultado;
+    }
+
     public async Task<List<VentasResponse>> ListarVentas() {
         List<VentasResponse> resultado = new List<VentasResponse>();
 
--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -24,11 +24,11 @@ public class VentasController : ControllerBase {
     //     return peticion.Exito.Equals( false ) ? Ok( new{ mensaje = peticion.Mensaje } ) : Ok( peticion );
     // }
 
-    // [HttpDelete("EliminarVentas/{id}")]
-    // public async Task<IActionResult> EliminarVentas( Guid id ) {
-    //     var peticion = await BLVentasAdmin.EliminarVentas( id );
-    //     return peticion.Exito.Equals( false ) ? Ok( new{ mensaje = peticion.Mensaje } ) : Ok( peticion );
-    // }
+    [HttpDelete("EliminarVentas/{id}")]
+    public async Task<IActionResult> EliminarVentas( Guid id ) {
+        var peticion = await BLVentasAdmin.EliminarVentas( id );
+        return peticion.Exito.Equals( false ) ? Ok( new{ mensaje = peticion.Mensaje } ) : Ok( peticion );
+    }
 
     [HttpGet("ListarVentas")]
     public async Task<IActionResult> ListarVentas() {
